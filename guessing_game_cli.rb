ad494fee440809db438dbfa1135764d1283d9484
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-07-10 22:54:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,13 @@ def run_guessing_game
   comp_number = generate_random_number
   prompt_user
   user_number = capture_user_input
-  compare_numbers(comp_number, user_number)
+  if comp_number == user_number
+    puts "You guessed the right number!"
+  elsif user_number == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{comp_number}."
+  end
 end
 
 def generate_random_number
@@ -19,11 +25,5 @@ def capture_user_input
 end
 
 def compare_numbers(comp_number, user_number)
-  if comp_number == user_number
-    puts "You guessed the right number!"
-  elsif user_number == "exit"
-    puts "Goodbye!"
-  else
-    puts "Sorry! The computer guessed #{comp_number}."
-  end
+
 end
